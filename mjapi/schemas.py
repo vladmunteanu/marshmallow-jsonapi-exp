992db11485d939b32e56996deacca63043a6fb39
Vlad <vladstefanmunteanu@gmail.com>
2022-09-05 12:04:56 +0300

    Added top level schema

--- mjapi/schemas.py
+++ mjapi/schemas.py
@@ -5,15 +5,42 @@ from marshmallow import Schema, SchemaOpts, fields
 from mjapi.fields import RelationshipType
 
 
+class ErrorObjectSchema(Schema):
+    id = fields.String()
+    links = fields.Dict()  # TODO: schema for links
+    status = fields.String()
+    code = fields.String()
+    title = fields.String()
+    detail = fields.String()
+    source = fields.Dict()  # TODO: schema for source
+    meta = fields.Dict()
+
+    def dump(self, *args, **kwargs):
+        """ Overwrite to remove empty fields. """
+        ret = super().dump(*args, **kwargs)
+        for field_name in self._declared_fields.keys():
+            if ret.get(field_name) is None:
+                ret.pop(field_name)
+        return ret
+
+
+class JSONAPIObjectSchema(Schema):
+    version = fields.String()
+
+
 class JSONAPISchema(Schema):
-    type: str
+    jsonapi_object_schema: t.Type[Schema] = JSONAPIObjectSchema
+    error_object_schema: t.Type[Schema] = ErrorObjectSchema
+    type: str = ''
 
     @classmethod
-    def get_jsonapi_schema(cls) -> t.Type[Schema]:
+    def get_jsonapi_resource_object_schema(cls) -> t.Type[Schema]:
         schema_declared_fields = cls._declared_fields.copy()
+        # using the id field that is defined on the schema
         schema_id_field = schema_declared_fields.pop('id')
-        schema_type_field = fields.Constant(getattr(cls, 'type', 'unknown'))
+        schema_type_field = fields.Constant(cls.type)
 
+        # add fields for attributes and relationships
         schema_attributes = {}
         schema_relationships = {}
         for field_name, field in schema_declared_fields.items():
@@ -22,7 +49,7 @@ class JSONAPISchema(Schema):
             else:
                 schema_attributes[field_name] = field
 
-        class _JSONAPISchema(Schema):
+        class ResourceObjectSchema(Schema):
 
             class Meta(SchemaOpts):
                 register = False
@@ -74,4 +101,59 @@ class JSONAPISchema(Schema):
 
                 return ret if many else ret[0]
 
-        return _JSONAPISchema
+        return ResourceObjectSchema
+
+    @classmethod
+    def get_jsonapi_top_level_schema(cls, many: bool = False) -> t.Type[Schema]:
+        resource_object_schema_cls = cls.get_jsonapi_resource_object_schema()
+
+        class TopLevelSchema(Schema):
+            class Meta(SchemaOpts):
+                register = False
+
+            data = fields.Nested(resource_object_schema_cls)
+            if many:
+                data = fields.List(data)
+            errors = fields.List(fields.Nested(cls.error_object_schema))
+            meta = fields.Dict()
+
+            # this is likely wrong possible solution: use a simple list
+            included = fields.List(fields.Nested(resource_object_schema_cls))
+
+            jsonapi = fields.Nested(cls.jsonapi_object_schema)
+
+            # TODO schema for links
+            links = fields.Dict(keys=fields.String(), values=fields.String())
+
+            def __init__(self, *, only=None, **kwargs):
+                new_only = [] if only else None
+                if only:
+                    for field_name in only:
+                        # this will get stripped when passed to nested schemas
+                        new_only.append(f'data.{field_name}')
+
+                    new_only += ['errors', 'meta', 'included', 'jsonapi', 'links']
+                super().__init__(only=new_only, **kwargs)
+
+            def get_attribute(self, obj: t.Any, attr: str, default: t.Any):
+                if attr == 'data' and not isinstance(obj, Exception):
+                    return obj
+                elif attr == 'errors' and isinstance(obj, Exception):
+                    # TODO support multiple errors
+                    return [obj]
+                elif attr == 'included':
+                    # TODO included data and deduplication
+                    return self.context.get('included_data', default)
+                elif attr == 'meta':
+                    return self.context.get('top_level_meta', default)
+                elif attr == 'jsonapi':
+                    return self.context.get('jsonapi_info', default)
+                return default
+
+            def load(self, *args, **kwargs):
+                """ Overwrite to flatten data. """
+                ret = super().load(*args, **kwargs)
+                ret.update(**ret.pop('data', {}))
+                return ret
+
+        return TopLevelSchema
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 def test_team_schema_jsonapi_simple(team_schema_cls, team_1):
-    team_schema_cls = team_schema_cls.get_jsonapi_schema()
+    team_schema_cls = team_schema_cls.get_jsonapi_resource_object_schema()
     serialized = team_schema_cls().dump(team_1)
     assert serialized == {
         'id': team_1.id,
@@ -11,7 +11,7 @@ def test_team_schema_jsonapi_simple(team_schema_cls, team_1):
 
 
 def test_user_schema_jsonapi_empty_relationships(user_schema_cls, user_1):
-    user_schema_cls = user_schema_cls.get_jsonapi_schema()
+    user_schema_cls = user_schema_cls.get_jsonapi_resource_object_schema()
     serialized = user_schema_cls().dump(user_1)
     assert serialized == {
         'id': user_1.id,
@@ -24,7 +24,7 @@ def test_user_schema_jsonapi_empty_relationships(user_schema_cls, user_1):
 
 
 def test_user_schema_jsonapi_relationship(user_schema_cls, user_1, user_2):
-    user_schema_cls = user_schema_cls.get_jsonapi_schema()
+    user_schema_cls = user_schema_cls.get_jsonapi_resource_object_schema()
     serialized = user_schema_cls().dump(user_2)
     assert serialized == {
         'id': user_2.id,
@@ -47,7 +47,7 @@ def test_user_schema_jsonapi_relationship(user_schema_cls, user_1, user_2):
 def test_user_schema_jsonapi_many_relationship(
         user_schema_cls, user_1, team_1, team_2, user_3,
 ):
-    user_schema_cls = user_schema_cls.get_jsonapi_schema()
+    user_schema_cls = user_schema_cls.get_jsonapi_resource_object_schema()
     serialized = user_schema_cls().dump(user_3)
     assert serialized == {
         'id': user_3.id,
@@ -80,7 +80,7 @@ def test_user_schema_jsonapi_many_relationship(
 
 
 def test_user_schema_dump_many(user_schema_cls, user_1, user_2):
-    user_schema_cls = user_schema_cls.get_jsonapi_schema()
+    user_schema_cls = user_schema_cls.get_jsonapi_resource_object_schema()
     serialized = user_schema_cls().dump([user_1, user_2], many=True)
     assert serialized == [
         {
@@ -111,7 +111,7 @@ def test_user_schema_dump_many(user_schema_cls, user_1, user_2):
 
 
 def test_user_schema_only(user_schema_cls, user_1, user_2):
-    user_schema_cls = user_schema_cls.get_jsonapi_schema()
+    user_schema_cls = user_schema_cls.get_jsonapi_resource_object_schema()
     serialized = user_schema_cls(only=['name']).dump(user_2)
     assert serialized == {
         'id': user_2.id,  # type is required
@@ -133,4 +133,200 @@ def test_user_schema_only(user_schema_cls, user_1, user_2):
                 }
             }
         }
+    }
+
+
+def test_top_level_schema(user_schema_cls, user_1):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    serialized = top_level_schema().dump(user_1)
+    assert serialized == {
+        'data': {
+            'id': user_1.id,
+            'type': 'users',
+            'attributes': {
+                'name': user_1.name,
+                'email': user_1.email,
+            },
+        },
+    }
+
+
+def test_top_level_schema_many(user_schema_cls, user_1, user_2):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema(many=True)
+    serialized = top_level_schema().dump([user_1, user_2])
+    assert serialized == {
+        'data': [
+            {
+                'id': user_1.id,
+                'type': 'users',
+                'attributes': {
+                    'name': user_1.name,
+                    'email': user_1.email,
+                },
+            },
+            {
+                'id': user_2.id,
+                'type': 'users',
+                'attributes': {
+                    'name': user_2.name,
+                    'email': user_2.email,
+                },
+                'relationships': {
+                    'referrer': {
+                        'data': {
+                            'id': user_1.id,
+                            'type': 'users'
+                        }
+                    }
+                }
+            },
+        ]
+    }
+
+
+def test_top_level_schema_errors(user_schema_cls):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    top_level_schema_many = user_schema_cls.get_jsonapi_top_level_schema(many=True)
+
+    class TestHttpException(Exception):
+        def __init__(
+                self, id: str = None, links: dict = None, status: str = None,
+                code: str = None, title: str = None, detail: str = None,
+                source: dict = None, meta: dict = None,
+        ):
+            self.id = id
+            self.links = links
+            self.status = status
+            self.code = code
+            self.title = title
+            self.detail = detail
+            self.source = source
+            self.meta = meta
+
+    exc = TestHttpException(id='exc-id-test', status='404', code='1234', detail='something went wrong')
+
+    serialized = top_level_schema().dump(exc)
+    serialized_many = top_level_schema_many().dump(exc)
+    assert serialized == serialized_many == {
+        'errors': [
+            {
+                'id': 'exc-id-test',
+                'status': '404',
+                'code': '1234',
+                'detail': 'something went wrong',
+            },
+        ]
+    }
+
+
+def test_top_level_schema_jsonapi_info(user_schema_cls, user_1):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    serialized = top_level_schema(context={'jsonapi_info': {'version': '1.0'}}).dump(user_1)
+    assert serialized == {
+        'jsonapi': {
+            'version': '1.0',
+        },
+        'data': {
+            'id': user_1.id,
+            'type': 'users',
+            'attributes': {
+                'name': user_1.name,
+                'email': user_1.email,
+            },
+        },
+    }
+
+
+def test_top_level_schema_only(user_schema_cls, user_1, user_2):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    serialized = top_level_schema(only=['name']).dump(user_2)
+    assert serialized == {
+        'data': {
+            'id': user_2.id,  # type is required
+            'type': 'users',  # id is required
+            'attributes': {
+                'name': user_2.name,
+            },
+        },
+    }
+
+    serialized = top_level_schema(only=['referrer']).dump(user_2)
+    assert serialized == {
+        'data': {
+            'id': user_2.id,  # type is required
+            'type': 'users',  # id is required
+            'relationships': {
+                'referrer': {
+                    'data': {
+                        'id': user_1.id,
+                        'type': 'users',
+                    }
+                }
+            }
+        },
+    }
+
+
+def test_top_level_schema_meta(user_schema_cls, user_1):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    serialized = top_level_schema(context={'top_level_meta': {'custom_field': 'custom_value'}}).dump(user_1)
+    assert serialized == {
+        'meta': {
+            'custom_field': 'custom_value',
+        },
+        'data': {
+            'id': user_1.id,
+            'type': 'users',
+            'attributes': {
+                'name': user_1.name,
+                'email': user_1.email,
+            },
+        },
+    }
+
+
+def test_top_level_schema_only_maintains_other_top_level_fields(user_schema_cls, user_2):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    serialized = top_level_schema(only=['name'], context={'jsonapi_info': {'version': '1.0'}}).dump(user_2)
+    assert serialized == {
+        'jsonapi': {
+            'version': '1.0',
+        },
+        'data': {
+            'id': user_2.id,
+            'type': 'users',
+            'attributes': {
+                'name': user_2.name,
+            },
+        },
+    }
+
+
+def test_load_top_level_schema(user_schema_cls, user_1, user_2):
+    top_level_schema = user_schema_cls.get_jsonapi_top_level_schema()
+    deserialized = top_level_schema().load(
+        {
+            'data': {
+                'id': user_2.id,
+                'type': 'users',
+                'attributes': {
+                    'name': user_2.name,
+                    'email': user_2.email,
+                },
+                'relationships': {
+                    'referrer': {
+                        'data': {
+                            'id': user_1.id,
+                            'type': 'users',
+                        }
+                    }
+                }
+            }
+        }
+    )
+    assert deserialized == {
+        'id': user_2.id,
+        'name': user_2.name,
+        'email': user_2.email,
+        'referrer': user_1.id,
     }
